00d56d7a568dadbf295b0c2758c9faffe47fb931
Paweł Fertyk <pfertyk@pfertyk.me>
2020-04-04 15:46:47 +0200

    Fix #3 allow mixed dice type in one roll

--- rpg_dice/__init__.py
+++ rpg_dice/__init__.py
@@ -1,12 +1,15 @@
 import random
 import re
 
-DIE_PATTERN = re.compile("(?P<count>\\d*)d(?P<value>\\d+)")
+SINGLE_DIE_PATTERN = re.compile("(?P<count>\\d*)d(?P<value>\\d+)")
 
 
 def roll(dice_str):
-    search = DIE_PATTERN.search(dice_str)
-    count = search.group("count")
-    count = int(count) if count else 1
-    value = int(search.group("value"))
-    return sum(random.randrange(value) + 1 for _ in range(count))
+    result = 0
+    for single_dice_str in dice_str.replace("\\s", "").split("+"):
+        search = SINGLE_DIE_PATTERN.search(single_dice_str)
+        count = search.group("count")
+        count = int(count) if count else 1
+        value = int(search.group("value"))
+        result += sum(random.randrange(value) + 1 for _ in range(count))
+    return result
